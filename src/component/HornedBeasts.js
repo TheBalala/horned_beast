767644f59c3a34874f41e2fa48031c61e8953a7c
Ali-coder1996 <alisalamehhjouj@gmail.com>
2021-08-02 15:59:33 +0300

    done the lab02

--- src/component/HornedBeasts.js
+++ src/component/HornedBeasts.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'
 
 class HornedBeasts extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             counter:0
+        }
+    }
+    counterHandler=()=>{(
+        this.setState({
+            counter:this.state.counter +1
+        })
+    )}
+    
     render() {
         return (
             
             <div>
                 <h2>{this.props.title}</h2>
                 <img className='img' src={this.props.imageUrl} alt={this.props.title} />
-                <p>{this.props.description}</p>
+                <p className='para'>{this.props.description}</p>
+                <i onClick={()=>this.counterHandler()} className='i'class="fas fa-heart"></i>
+                <span>{this.state.counter}</span>
             </div>
             
         )
